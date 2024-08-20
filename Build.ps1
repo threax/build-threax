bf0f284ba36dc88213972c80ea7b66ed2b8e39ff
Andrew Piper <andrew@threax.com>
2024-08-20 08:14:42 -0400

    Update to .net 8.0 and node 20.

--- Build.ps1
+++ Build.ps1
@@ -2,6 +2,6 @@ $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 
 Push-Location $scriptPath
 
-wsl sudo docker build -t threax/build-dotnet:6.0 .
+docker build -t threax/build-dotnet:8.0 .
 
 Pop-Location
